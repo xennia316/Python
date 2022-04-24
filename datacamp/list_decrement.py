aeff6217a3c6c4fd97e4ac3676def3f0057274ce
xennia316 <xennia316@gmail.com>
2022-04-24 19:04:50 +0100

    deleting elements from a list

---
+++ datacamp/list_decrement.py
@@ -0,0 +1,8 @@
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+del(areas[-4:-2])
+
+# Note that after deleting one elemt the other elements after it assume it's index
