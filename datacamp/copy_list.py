04aaed6e3d1f4c00498c99392292a61b489986f4
xennia316 <xennia316@gmail.com>
2022-04-24 19:09:49 +0100

    copying a list to new  list without sharing memory

---
+++ datacamp/copy_list.py
@@ -0,0 +1,13 @@
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas[:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+
+# A change in areas does not affect areas_copy and vice versa because they do not point to thesame place in memory
