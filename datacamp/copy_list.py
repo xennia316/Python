b051d4254b1bdccb1bde80d557e84f667e03df39
xennia316 <xennia316@gmail.com>
2022-04-25 20:27:23 +0100

    additional print line for easy comprehension

--- datacamp/copy_list.py
+++ datacamp/copy_list.py
@@ -9,5 +9,6 @@ areas_copy[0] = 5.0
 
 # Print areas
 print(areas)
+print(areas_copy)
 
 # A change in areas does not affect areas_copy and vice versa because they do not point to thesame place in memory
