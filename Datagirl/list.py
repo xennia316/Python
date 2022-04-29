a87b925bf2e0b28cb92061322f60edffcc7c5633
xennia316 <xennia316@gmail.com>
2022-04-29 03:40:12 +0100

    creating list datatype

---
+++ Datagirl/list.py
@@ -0,0 +1,4 @@
+# Create with []
+empty_list = []
+week_days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+randomness = ["Punxsatawney", {"groundhog": "Phil"}, "Feb. 2"]
