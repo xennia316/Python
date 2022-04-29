98c738ffd195342f494e584c536f5b7b7303690f
xennia316 <xennia316@gmail.com>
2022-04-29 03:44:50 +0100

    converting to list and spliting lists

--- Datagirl/list.py
+++ Datagirl/list.py
@@ -2,3 +2,18 @@
 empty_list = []
 week_days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 randomness = ["Punxsatawney", {"groundhog": "Phil"}, "Feb. 2"]
+
+# Converting other datatypes to list with list()
+another_empty_list = list()
+
+print(another_empty_list)
+
+list('cats')
+
+# Creating a string with split
+talk_like_a_pirate_day = '9/19/2019'
+talk_like_a_pirate_day.split('/')
+
+print(talk_like_a_pirate_day.split('/'))
+#['9', '19', '2019']
+
