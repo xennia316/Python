ea968ad94f7e9368d2f8723c20b1c528c5f0d688
xennia316 <xennia316@gmail.com>
2022-04-29 03:23:25 +0100

    combining, duplicating and comparing tuples

--- Datagirl/tuples.py
+++ Datagirl/tuples.py
@@ -7,3 +7,27 @@ print(icecream, password)
 # Creating new tuples with tuple() command
 marx_list =[ 'hello', 'how', 'are', 'you']
 tuple(marx_list)
+
+# Combining two or more tuples with +
+('hello') + ('how', 'are', 'you')
+
+marx_list + ('doing')
+
+print(marx_list)
+
+#Duplicating items with *
+mangoes = ('mango') * 3
+print(mangoes) 
+
+# Comparing Tuples with == sign. Returns true or false
+a = (7, 2)
+b = 7, 2, 9
+
+print(a == b) #false
+
+print(a <= b) #true
+
+print(a < b) #true
+
+print(a > b) #false
+
