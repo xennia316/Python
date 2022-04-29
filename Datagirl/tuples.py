b01f37582198ea5740dd610a8190b66de7a144e4
xennia316 <xennia316@gmail.com>
2022-04-29 03:16:05 +0100

    creating a new tuple

--- Datagirl/tuples.py
+++ Datagirl/tuples.py
@@ -3,3 +3,7 @@ icecream = 'tuttifrutti'
 password, icecream = icecream, password
 
 print(icecream, password)
+
+# Creating new tuples with tuple() command
+marx_list =[ 'hello', 'how', 'are', 'you']
+tuple(marx_list)
