f385113414281ac8dc362534cbeebff8ea1c633a
xennia316 <xennia316@gmail.com>
2022-05-03 22:00:52 +0100

    comments addded

--- Datagirl/assignment3.py
+++ Datagirl/assignment3.py
@@ -1,5 +1,11 @@
+# List of next five years after birth year
 years_list = [2003, 2004, 2005, 2006, 2007, 2008]
 
+# Find the year with your third birthday
 print(years_list[3])
 
+# In what year were you the oldest?
 print(years_list[-1])
+
+things = ["mozarella", "cinderella", "salmonella"]
+
