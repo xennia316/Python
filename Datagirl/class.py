3a73e90703f44574dc822ce24233d2c23472f390
xennia316 <xennia316@gmail.com>
2022-05-06 17:55:09 +0100

    inheritance

--- Datagirl/class.py
+++ Datagirl/class.py
@@ -1,11 +1,10 @@
-from ast import Pass
-from urllib.parse import ParseResultBytes
 
 
 class Cat():
-    Pass
+    pass
 
-a_cat =  Cat()
+
+a_cat = Cat()
 another_cat = Cat()
 
 print(type(Cat))
@@ -15,10 +14,25 @@ a_cat.name = "Sonia"
 
 print(a_cat.name)
 
+
 class NewCat():
     def __init__(self, name):
         self.name = name
 
+
 a_NewCat = NewCat("Edwin")
 
 print(a_NewCat.name)
+
+
+class Car():
+    def exclaim(self):
+        print("I am a car!")
+
+
+class Yoga(Car):
+    pass
+
+
+a_yoga = Yoga()
+a_yoga.exclaim()
