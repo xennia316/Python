4906575ac02683ee8b0da880df10b68589ee74a6
xennia316 <xennia316@gmail.com>
2022-05-07 22:37:06 +0100

    switch case

--- datacamp/tryout.py
+++ datacamp/tryout.py
@@ -2,17 +2,18 @@ a = float(input('First: '))
 b = float(input('Second: '))
 op = input('Operation(sum/sub/mul/div)')
 
-if op == 'sum':
-    print(f'a + b = {a + b}')
+match op:
+    case 'sum':
+        print(f'a + b = {a + b}')
 
-elif op == 'sub':
-    print(f'a - b = {a - b}')
+    case 'sub':
+        print(f'a - b = {a - b}')
 
-elif op == 'mul':
-    print(f'a *  b = {a * b}')
+    case 'mul':
+        print(f'a *  b = {a * b}')
 
-elif op == 'div':
-    print(f'a / b = {a / b}')
+    case 'div':
+        print(f'a / b = {a / b}')
 
-else:
-    print('Invalid')
+    case _:
+        print('Invalid')
