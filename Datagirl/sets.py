b01f11899bd232a809cbd5a21a71d24935f775f0
xennia316 <xennia316@gmail.com>
2022-05-08 21:47:29 +0100

    string to set

--- Datagirl/sets.py
+++ Datagirl/sets.py
@@ -5,3 +5,6 @@ even_numbers = {0, 2, 4, 6, 8}
 odd_numbers = {1, 3, 5, 7, 9}
 
 print(odd_numbers)
+
+# Creating a set from a string .... It counts only one occurrence of a character
+print(set('letters'))
