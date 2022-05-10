2b20306eb66fa62ad7eacc4688ebe0c1cef5091f
xennia316 <xennia316@gmail.com>
2022-05-10 18:33:17 +0100

    printing args as a tuple

--- Datagirl/functions.py
+++ Datagirl/functions.py
@@ -97,4 +97,14 @@ def menu(wine, entree, dessert='pudding'):
     return {'wine': wine, 'entree': entree, 'dessert': dessert}
 
 
-menu('chardonnay', 'chicken')
+print(menu('chardonnay', 'chicken')
+      )
+
+print(menu('dunkelfelder', 'duck', 'doughnut'))
+
+
+def print_args(*args):
+    print('Positional tuple:', args)
+
+
+print(print_args(3, 2, 1, 'wait!', 'uh...'))
