b0eaeb3f9026c9a33ee563bd87270d2a90566e6e
xennia316 <xennia316@gmail.com>
2022-05-11 19:29:35 +0100

    exercise 2 - 1

---
+++ Datagirl/exercises2.py
@@ -0,0 +1,11 @@
+print('________________________')
+print('Exercise 1')
+print('________________________')
+
+
+def takes_two_args(name, age):
+    print(name)
+    print(age)
+
+
+(takes_two_args('Sonia', 15))
