9563ca1220e8eb6c7f340a7a297c8494469d5bca
xennia316 <xennia316@gmail.com>
2022-05-11 19:02:01 +0100

    exercise 2

--- Datagirl/exercises.py
+++ Datagirl/exercises.py
@@ -8,3 +8,18 @@ a_list = [1, 2, 3, 4, 5]
 print(type(a_list))
 
 print("The sum of this list is:", sum(a_list[:]))
+print('\n')
+print('______________________________________________')
+print('Exercise 2')
+print('______________________________________________')
+print('\n')
+
+
+def multiplier(lists):
+    result = 1
+    for num in lists:
+        result *= num
+    return result
+
+
+print(multiplier(a_list))
