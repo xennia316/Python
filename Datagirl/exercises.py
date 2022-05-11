8a10e047c74d92a103fe45090d3486b34ed7668a
xennia316 <xennia316@gmail.com>
2022-05-11 19:01:15 +0100

    exercise 1

---
+++ Datagirl/exercises.py
@@ -0,0 +1,10 @@
+# Exercise 1
+import random
+from operator import indexOf
+from tokenize import String
+
+
+a_list = [1, 2, 3, 4, 5]
+print(type(a_list))
+
+print("The sum of this list is:", sum(a_list[:]))
