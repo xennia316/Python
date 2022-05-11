f96df36562145e2d02e3b81b4a775d6abf44a859
xennia316 <xennia316@gmail.com>
2022-05-11 19:56:22 +0100

    exercises 5 to 9

--- Datagirl/exercises2.py
+++ Datagirl/exercises2.py
@@ -1,3 +1,7 @@
+import copy
+from copy import deepcopy
+
+
 print('________________________')
 print('Exercise 1')
 print('________________________')
@@ -34,7 +38,7 @@ def calculation(arg1, arg2):
 print(calculation(20, 5))
 
 print('________________________')
-print('Exercise 3')
+print('Exercise 4')
 print('________________________')
 
 
@@ -45,3 +49,69 @@ def show_employee(name, salary=9000):
 
 show_employee("Ben", 12000)
 show_employee("Jessa")
+
+print('________________________')
+print('Exercise 5')
+print('________________________')
+
+
+def outer(a, b):
+    def inner(a, b):
+        return a + b
+    return inner(a, b) + 5
+
+
+print(outer(1, 2))
+
+print('________________________')
+print('Exercise 6')
+print('________________________')
+
+
+def summer(a):
+    if a <= 1:
+        return a
+    else:
+        return a + summer(a - 1)
+
+
+print(summer(9))
+
+print('________________________')
+print('Exercise 7')
+print('________________________')
+
+
+def display_student(name, age):
+    name = name
+    age = age
+    return name, age
+
+
+def show_student(name, age):
+    pass
+
+
+show_student = copy.deepcopy(display_student)
+
+print('________________________')
+print('Exercise 8')
+print('________________________')
+
+for numbers in range(4, 31):
+    if numbers % 2 == 0:
+        print(numbers)
+
+print('________________________')
+print('Exercise 9')
+print('________________________')
+
+
+def largest(list):
+    new_list = sorted(list)
+    return new_list[-1]
+
+
+random_list = [2, 3, 1, 7, 0, 9, 20]
+
+print(largest(random_list))
