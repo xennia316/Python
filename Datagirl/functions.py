5f7440f04ac9de0b8ce2450a24f567aa2e52626a
xennia316 <xennia316@gmail.com>
2022-05-11 03:53:41 +0100

    keyword-on;y arguments

--- Datagirl/functions.py
+++ Datagirl/functions.py
@@ -127,3 +127,19 @@ def print_kwargs(**kwargs):
 
 
 print_kwargs(wine='merlot', entree='mutton', dessert='macaroon')
+
+# Keyword-Only Arguments
+
+
+def print_data(data, *, start=0, end=100):
+    for value in (data[start:end]):
+        print(value)
+
+
+data = ['a', 'b', 'c', 'd', 'e', 'f']
+print('*************************')
+print_data(data)
+print('*************************')
+print_data(data, start=3)
+print('*************************')
+print_data(data, end=2)
