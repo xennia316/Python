0fbd275863e755b637062bf1e88e272ac60e1d59
xennia316 <xennia316@gmail.com>
2022-05-15 23:29:59 +0100

    exercise 3 attempted

--- Datagirl/dicts&sets_exercises.py
+++ Datagirl/dicts&sets_exercises.py
@@ -2,3 +2,12 @@ e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
 
 # Exercise 2
 print(e2f['walrus'])
+
+# Exercise 3
+# for items in e2f.items():
+#     for value in items.value():
+#         for key in items.keys():
+#             f2e = {value: key}
+#         print(f2e)
+
+# Exercise 4
