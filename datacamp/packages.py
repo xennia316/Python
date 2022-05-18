06a7ca564148a21281c8393e2f589df5c54337b5
xennia316 <xennia316@gmail.com>
2022-05-18 18:05:00 +0100

    math library

---
+++ datacamp/packages.py
@@ -0,0 +1,16 @@
+# Definition of radius
+import math
+r = 0.43
+
+# Import the math package
+
+# Calculate C
+pi = math.pi
+C = 2 * pi * r
+
+# Calculate A
+A = pi*r*r
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
