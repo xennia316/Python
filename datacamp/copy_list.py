ce510ca33b3544b132d71d5aaa1eb12fb6b023b8
Sonia Amehmbo <87652337+xennia316@users.noreply.github.com>
2022-06-21 20:20:13 +0100

    Update copy_list.py

--- datacamp/copy_list.py
+++ datacamp/copy_list.py
@@ -11,4 +11,4 @@ areas_copy[0] = 5.0
 print(areas)
 print(areas_copy)
 
-# A change in areas does not affect areas_copy and vice versa because they do not point to thesame place in memory
+# A change in areas does not affect areas_copy and vice versa because they do not point to thesame place in memory.
